97766995778f32f5b1aa2306a784c2ff11cf4ca2
MoayadI <moayad@hashicorp.com>
2020-09-01 00:06:48 +0800

    first commit

--- cloudflare.tf
+++ cloudflare.tf
@@ -10,12 +10,19 @@ variable "apikey" {
   type = string
 }
 
-resource "cloudflare_zone" "imoayad" {
-    zone = var.domain
+# resource "cloudflare_zone" "imoayad" {
+#     zone = var.domain
+# }
+
+data "cloudflare_zones" "zone" {
+  filter {
+    name = "imoayad.me"
+  }
+  
 }
 
 resource "cloudflare_record" "vaultserver" {
-  zone_id = cloudflare_zone.imoayad.id
+  zone_id = data.cloudflare_zones.zones[0].id
   name    = "vault.imoayad.me"
   value   = "34.227.192.182"
   type    = "A"
