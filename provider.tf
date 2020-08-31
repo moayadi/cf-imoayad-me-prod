ccd5990f26b376e08da53506c65bd70436e3bcad
MoayadI <moayad@hashicorp.com>
2020-08-31 23:51:52 +0800

    first commit

--- provider.tf
+++ provider.tf
@@ -1,13 +1,11 @@
-# terraform {
-#   required_providers {
-#     cloudflare = {
-#       source = "cloudflare/cloudflare"
-#       version = "2.10.1"
-#       email = var.username
-#       api_key = var.apikey
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "2.10.1"
+    }
+  }
+}
 
 provider "cloudflare" {
   email = var.username
