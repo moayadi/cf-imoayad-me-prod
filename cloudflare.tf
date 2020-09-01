3e26d6119ec4fe6b8ca65d6aa07ac944f88cfccf
moayadi <42159604+moayadi@users.noreply.github.com>
2020-09-01 14:32:33 +0800

    Update cloudflare.tf

--- cloudflare.tf
+++ cloudflare.tf
@@ -39,11 +39,11 @@ resource "cloudflare_record" "vaultserver1" {
   proxied = true
 }
 
-# resource "cloudflare_record" "vaultserver2" {
-#   zone_id = data.cloudflare_zones.zone.zones[0].id
-#   name    = "vault2.imoayad.me"
-#   value   = "34.227.192.182"
-#   type    = "A"
-#   ttl     = 1
-#   proxied = false
-# }
+resource "cloudflare_record" "vaultserver2" {
+  zone_id = data.cloudflare_zones.zone.zones[0].id
+  name    = "vault2.imoayad.me"
+  value   = "34.227.192.182"
+  type    = "A"
+  ttl     = 1
+  proxied = false
+}
